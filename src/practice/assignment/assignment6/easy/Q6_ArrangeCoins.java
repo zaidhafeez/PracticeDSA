f44e9538718e067064e6d860da3a747861a7e9d9
Zaid <zaid.kpj@gmail.com>
2022-02-11 00:56:04 +0530

    BinarySearch/Q6_ArrangeCoins added

--- src/practice/assignment/assignment6/easy/Q6_ArrangeCoins.java
+++ src/practice/assignment/assignment6/easy/Q6_ArrangeCoins.java
@@ -4,15 +4,29 @@ public class Q6_ArrangeCoins {
 
     static int arrangeCoins(int noOfCoins){
 
-        for (int i = 0; i < noOfCoins; i++) {
-            
+        int start = 1;
+        int end = noOfCoins;
+
+        while(start <= end){
+
+            int mid = start + (end - start) / 2;
+            int coinFilled = mid * (mid + 1) / 2;
+
+            if(coinFilled <= noOfCoins){
+                start = mid + 1;
+            }
+            else{
+                end = mid - 1;
+            }
         }
 
-        return -1;
+        return start - 1;
         
     }
     
     public static void main(String[] args) {
+
+        System.out.println(arrangeCoins(1));
         
     }
     
